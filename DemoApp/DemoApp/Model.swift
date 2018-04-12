581e9de75e7cb485cc9626febaa841153a8e9ac6
Nguyễn Hữu Dũng <dungnh@iotech.vn>
2018-04-12 13:55:19 +0700

    model

---
+++ DemoApp/DemoApp/Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  DemoApp
+//
+//  Created by Nguyễn Hữu Dũng on 4/12/18.
+//  Copyright © 2018 Nguyễn Hữu Dũng. All rights reserved.
+//
+
+import UIKit
+
+class Model: NSObject {
+
+    override init() {
+        print("abc")
+    }
+    
+}
--- DemoApp/DemoApp/ViewController.swift
+++ DemoApp/DemoApp/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
         print("git merge")
         print("tôi yêu em")
         
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
