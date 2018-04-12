d330ac63bd96e8d80ea169229c37c59ce8906290
Nguyễn Hữu Dũng <dungnh@iotech.vn>
2018-04-12 11:27:21 +0700

    merge toi yeu em

--- DemoApp/DemoApp/ViewController.swift
+++ DemoApp/DemoApp/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         print("git init")
         print("git push")
         print("git merge")
+        print("tôi yêu em")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
